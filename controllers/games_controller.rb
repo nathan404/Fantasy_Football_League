36d70a054ebe396b664cddf3ff17846d7cb104d1
Nathan <nathankirley@gmail.com>
2020-04-25 15:21:22 +0100

    index for game views

--- controllers/games_controller.rb
+++ controllers/games_controller.rb
@@ -4,3 +4,8 @@ require('pry-byebug')
 require_relative('../models/team.rb')
 require_relative('../models/game.rb')
 also_reload('../models/*')
+
+get '/games' do
+    @games = Game.all()
+    erb(:'games/index')
+end
